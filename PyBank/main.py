89bfdb1f802b43a2f246f4ff1917062764160ce5
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-04-05 14:24:58 -0400

    All Notebooks removed, Final Submissions prepared

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,5 @@
 ## OMIMP
-## ANDREW ANASTSIADES
+## Andrew Anastasiades
 
 #  O   M   I   M   P  
 #  H   A       I   A  
@@ -16,7 +16,7 @@ import csv
 budget_data_path = os.path.join("Resources", "budget_data.csv") #Budget Data File Path
 results_file_path = os.path.join("Analysis","Financial Analysis.txt")
 
-## READ FILE
+##READ FILE
 data_list = list() #List to hold rows #OMIMP
 with open(budget_data_path) as budget_file:
     csvreader = csv.reader(budget_file, delimiter=',') #Reader Object
@@ -30,35 +30,41 @@ with open(budget_data_path) as budget_file:
 month_list = [row[0] for row in data_list] #MONTHS
 income_values = [int(row[1]) for row in data_list] #INCOME
 changes = [income_values[i]-income_values[i-1] for i in range(1, len(income_values))] #CHANGES
-# Return Values
 total_months = len(month_list)
 total_income = sum(income_values)
 average_change = sum(changes) / len(changes)
-# NOTE: you have to add one to month index bcause first month has no change
+# you have to add one to month index bcause first month has no change
 greatest_increase = [month_list[changes.index(max(changes))+1], max(changes)] 
-greatest_decrease = [month_list[changes.index(min(changes))+1], min(changes]
+greatest_decrease = [month_list[changes.index(min(changes))+1], min(changes)]
 
-## WRITE RESULTS 
+##WRITE RESULTS 
 with open(results_file_path, "w") as results:
     
     results.write(f"Financial Analysis")
+    results.write("\n")
     print(f"Financial Analysis")
     
     results.write(f"--------------------------")
+    results.write("\n")
     print(f"--------------------------")
     
     results.write(f"Total Months: {total_months}")
+    results.write("\n")
     print(f"Total Months: {total_months}")
     
     results.write(f"Total: {total_income}")
+    results.write("\n")
     print(f"Total: ${total_income}")
     
     results.write(f"Average Change: ${average_change}")
+    results.write("\n")
     print(f"Average Change: ${average_change}")
     
     results.write(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
+    results.write("\n")
     print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
     
     results.write(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
+    results.write("\n")
     print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
-    
+## END
---
+++ PyPoll/main.py
@@ -0,0 +1,78 @@
+## OMIMP
+## Andrew Anastasiades
+
+#  O   M   I   M   P  
+#  H   A       I   A  
+#      N       S   N
+#              S   D
+#                  A
+#                  S
+
+## DEPENDENCIES
+import os
+import csv
+
+## FILE PATHS
+election_data_path = os.path.join("Resources", "election_data.csv") #Election Data File Path
+results_file_path = os.path.join("Analysis","Election Results.txt")
+
+##READ FILE
+data_list = list() #List to hold rows #OMIMP
+with open(election_data_path) as election_file:
+    csvreader = csv.reader(election_file, delimiter=',') #Reader Object
+    header = next(csvreader) # Get Column Heads #OMIMP
+    for row in csvreader:
+        data_list.append(row)
+    #Now we can close File
+
+## ANALYZE DATA
+# List Comprehensions to make life easier
+id_list = [row[0] for row in data_list]
+county_list = [row[1] for row in data_list]
+cand_list = [row[2] for row in data_list]
+#Results
+total_votes = len(data_list) #totals
+candidates = list(set(cand_list)) #Make a list of unique Candidates
+votes_by_cand = [cand_list.count(candidate) for candidate in candidates] #Make a list of vote totals
+percent_by_cand = [(votes/total_votes)*100 for votes in votes_by_cand] #Make a list of %s
+#Let's Consolidate the lists so we can sort (also I am now formatting my %s)
+result_list = [[candidates[i], votes_by_cand[i], "{:.3f}".format(percent_by_cand[i])] for i in range(len(candidates))]
+sorted_results = sorted(result_list, key=lambda x : x[1], reverse=True) #Ok, I admit we haven't covered lamba yet
+winner = sorted_results[0][0]
+
+##WRITE RESULTS 
+with open(results_file_path, "w") as results:
+    
+    results.write(f"Election Results")
+    results.write("\n")
+    print(f"Election Results")
+    
+    results.write(f"--------------------------")
+    results.write("\n")
+    print(f"--------------------------")
+    
+    results.write(f"Total Votes: {total_votes}")
+    results.write("\n")
+    print(f"Total Votes: {total_votes}")
+    
+    results.write(f"--------------------------")
+    results.write("\n")
+    print(f"--------------------------")
+    
+    for each in sorted_results:
+        results.write(f"{each[0]}: {each[2]}% ({each[1]})")
+        results.write("\n")
+        print(f"{each[0]}: {each[2]}% ({each[1]})")
+
+    results.write(f"--------------------------")
+    results.write("\n")
+    print(f"--------------------------")
+    
+    results.write(f"Winner: {winner}")
+    results.write("\n")
+    print(f"Winner: {winner}")
+    
+    results.write(f"--------------------------")
+    results.write("\n")
+    print(f"--------------------------")
+## END
